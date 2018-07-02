57fbe8bc2a0ff0353aacdd4b737a812ebf55ed98
Jakub Jezek <jezschaj@gmail.com>
2018-07-02 09:41:21 +0200

    convert mindbender onto magiclab pipeline

--- avalon-local.bat
+++ avalon-local.bat
@@ -5,9 +5,8 @@ set AVALON_LAUNCHER=G:\CODE\github\avalon-magiclab\git\avalon-launcher
 set AVALON_PROJECTS=G:\CODE\github\avalon-magiclab\git\avalon-examples
 set AVALON_CONFIG=polly
 set AVALON_LABEL=polly
-/*set AVALON_SENTRY=*/
 set AVALON_MONGO=mongodb://localhost:27017
-
+set AVALON_DEBUG=True
 set PYTHONPATH=G:\CODE\github\avalon-magiclab\git\mindbender-config;%PYTHONPATH%
 
 python G:\CODE\github\avalon-magiclab\avalon.py %*
---
+++ test_qt5_in_maya.py
@@ -0,0 +1,7 @@
+# https://pyblish.gitbooks.io/developer-guide/content/setting_up.html
+# https://legacy.gitbook.com/book/pyblish/developer-guide/details
+
+from pyblish_qml import api, show
+api.register_python_executable("C:/Users/jezsc/AppData/Local/conda/conda/envs/python27/python.exe")
+api.register_pyqt5("C:/Users/jezsc/AppData/Local/conda/conda/envs/python27/Lib/site-packages/PyQt5")
+show()
