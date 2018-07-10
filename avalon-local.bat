38268b16cce245cccec53545038cb521a11de151
Jakub Jezek <jezschaj@gmail.com>
2018-07-10 23:09:35 +0200

    fixing paths in avalon-local

--- avalon-local.bat
+++ avalon-local.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set AVALON_CORE=G:\CODE\github\avalon-magiclab\git\avalon-core
-set AVALON_LAUNCHER=G:\CODE\github\avalon-magiclab\git\avalon-launcher
-set AVALON_PROJECTS=G:\CODE\github\avalon-magiclab\git\avalon-examples
+set AVALON_CORE=G:\CODE\github\pipeline\avalon-magiclab\git\avalon-core
+set AVALON_LAUNCHER=G:\CODE\github\pipeline\avalon-magiclab\git\avalon-launcher
+set AVALON_PROJECTS=G:\CODE\github\pipeline\avalon-magiclab\git\avalon-examples
 set AVALON_CONFIG=polly
 set AVALON_LABEL=polly
 set AVALON_MONGO=mongodb://localhost:27017
 set AVALON_DEBUG=True
-set PYTHONPATH=G:\CODE\github\avalon-magiclab\git\mindbender-config;%PYTHONPATH%
+set PYTHONPATH=G:\CODE\github\pipeline\avalon-magiclab\git\mindbender-config;%PYTHONPATH%
 
-python G:\CODE\github\avalon-magiclab\avalon.py %*
+python G:\CODE\github\pipeline\avalon-magiclab\avalon.py %*
